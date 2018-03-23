ff05e259ef4c23d5589261f42c25e7b6d62a4eb2
Jarius Hayes <jarius_l_hayes@homedepot.com>
2018-03-23 09:26:19 -0400

    all my code

---
+++ src/app/userlist/userlist.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userlist',
+  templateUrl: './userlist.component.html',
+  styleUrls: ['./userlist.component.css']
+})
+export class UserlistComponent implements OnInit {
+
+  users = [
+    {name: 'John'},
+    {name: 'Peter'},
+    {name: 'Jay'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
